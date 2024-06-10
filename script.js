7307ecb3a996198febfcdd9345ad5fd7ce6162b3
Aleksei Korolyov <a97415393@gmail.com>
2024-06-10 14:14:09 +0300

    clock

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const element=document.getElementById(clock1);
+
+const Element = document.getElementById('clock');
+
+class Clock {
+    constructor(elementId, offset, timeZone) {
+        this.elementId=elementId;
+        this.offset=offset;
+        this.timeZone=timeZone;
+    }
+}
+
+let clock1 = new Clock("clock1", 0, "UTC");
+let clock2 = new Clock("clock2", 180, "Europe/Minsk");
+console.log(clock1);
+console.log(clock2);
