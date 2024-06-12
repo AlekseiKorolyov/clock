c74060d00eff1d685d046278d0829afa67057453
Aleksei Korolyov <a97415393@gmail.com>
2024-06-12 13:36:25 +0300

    clock

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 
-class displai {
-    constructor(hours, minutes, seconds) {
-        this.hours=date.getHours();
-        this.minutes=getMinutes();
-        this.seconds=getSeconds();
-    }
-}
+// const time = document.getElementById('clock');
 
-let date = new date
-alert(date);
 
-const Element = document.getElementById('clock');
+// class clock {
+//     constructor(elementId, offset, timeZone) {
+//         this.elementId=elementId;
+//         this.offset=offset;
+//         this.timeZone=timeZone;
+//     }
+// }
 
-class clock {
-    constructor(elementId, offset, timeZone) {
-        this.elementId=elementId;
-        this.offset=offset;
-        this.timeZone=timeZone;
-    }
-}
+// let clock1 = new clock("clock1", 0, "UTC");
+// let clock2 = new clock("clock2", 180, "Europe/Minsk");
 
-let clock1 = new clock("clock1", 0, "UTC");
-let clock2 = new clock("clock2", 180, "Europe/Minsk");
-console.log(clock1);
-console.log(clock2);
 
-element.innerHTML = "clock"
+// time.innerHTML = "clock1"
+
+// let date = new Date();
+// let hours = date.getHours();
+// let minutes = date.getMinutes();
+// let seconds = date.getSeconds();
+// alert ( hours );
+
+function clock(){
+    let date = new Date(),
+           hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
+           minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
+           seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+    document.getElementById('clock').innerHTML = hours + ':' + minutes + ':' + seconds;
+  }
+  setInterval(clock, 1000);
+  clock();
